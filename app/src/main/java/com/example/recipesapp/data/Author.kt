26b1f58410536990b40e98f204c1e8e7cb6d1c5f
DaniloEspinoza1996 <despinozap@ripley.com>
2022-03-10 17:56:27 -0300

    Add room database

---
+++ app/src/main/java/com/example/recipesapp/data/Author.kt
@@ -0,0 +1,7 @@
+package com.example.recipesapp.data
+
+data class Author(
+    val name: String?,
+    val nickname: String,
+    val email: String?,
+)
---
+++ app/src/main/java/com/example/recipesapp/data/Ingredient.kt
@@ -0,0 +1,7 @@
+package com.example.recipesapp.data
+
+data class Ingredient(
+    var name: String = "",
+    var quantity: Long? = null,
+    var unit: String? = null
+)
